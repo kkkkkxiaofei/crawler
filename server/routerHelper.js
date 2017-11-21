2e284a9035ebb7473bc241f1d6a6e53322279d4e
Xiaofei <xifzhang@thoughtworks.com>
2017-11-21 16:45:19 +0800

    successfully crawler for http://www.iproperty.com.my/sale/all-residential

--- server/routerHelper.js
+++ server/routerHelper.js
@@ -1,14 +1,41 @@
 import express from 'express';
+import Crawler from 'crawler';
+import _ from 'lodash';
+
+const parseData = ({$, body}) => {
+    var data = _.map($(body).find(('ul.listing-list > li')), function(elem) {
+        var $listing = $(elem);
+        var imgSrc = $listing.find('.slick-active img').attr('src');
+        var location = $listing.find('.row-one-left-col-listing-location').text();
+        return {
+            imgSrc,
+            location
+        };
+    });
+    return data;
+};
+
+const startCrawling = (request, response) => {
+    const crawler = new Crawler({
+        rateLimit: 1000, // `maxConnections` will be forced to 1
+        callback(err, res, done) {
+            done();
+            if (err) {
+                console.log('failed caused by - ', err);
+                response.status(500).end();
+            } else {
+                response.status(200).send(parseData(res));
+            }
+        }
+    });
+    const destination = request.query.uri;
+    destination && crawler.queue(destination);
+};
 
 const getRouter = () => {
     const router = express.Router();
     router.get('/start', (req, res) => {
-        try {
-            res.status(200).end();
-        } catch(e) {
-            console.log('failed caused by - ', e.message);
-            res.status(500).end();
-        }
+        startCrawling(req, res);
     });
     return router;
 };
